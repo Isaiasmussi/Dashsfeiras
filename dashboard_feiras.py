2ed21d2b22424ecef6abff2017d0a8729fdb4597
Isaiasmussi <isaiaslorandi@gmail.com>
2025-07-15 15:17:14 -0300

    Update dashboard_feiras.py

--- dashboard_feiras.py
+++ dashboard_feiras.py
@@ -1,4 +1,3 @@
-import os
 import pandas as pd
 import streamlit as st
 import folium
@@ -17,40 +16,39 @@ def carregar_e_limpar_dados():
     """
     Carrega os dados diretamente do código, eliminando a necessidade de um arquivo externo.
     """
-    # Dados da planilha inseridos diretamente no código
-    dados_string = """
-    Mês,Evento,Foco,Data,Cidade,UF
-    Janeiro,AgroShow Copagril 2026,Agronegócio,14 a 16,Marechal Cândido Rondon,PR
-    Janeiro,COOLACER 2026,Tecnologia,28 e 29,Lacerdópolis,SC
-    Janeiro,Dinetec,Tecnologia,,Canarana,MT
-    Janeiro,Fertilizer Latino Americano,Fertilizantes,,Rio de Janeiro,RJ
-    Fevereiro,Sealba Show 2026,Agronegócio,04 a 07,Itabaiana,SE
-    Fevereiro,Show Safra BR 163,Tecnologia,18 a 21,Lucas do Rio Verde,MT
-    Fevereiro,Show Tecnológico de Verão,Tecnologia,20 a 22,Ponta Grossa,PR
-    Fevereiro,Copla Campo,Agronegócio,21 a 23,Piracicaba,SP
-    Fevereiro,Show Rural Coopavel,Tecnologia,,"Cascavel",PR
-    Março,Expodireto Cotrijal,Agronegócio,03 a 07,Não-Me-Toque,RS
-    Março,Expo-Uva,Fruticultura,06 a 09,Jundiaí,SP
-    Março,Farm Show,Tecnologia,11 a 14,Primavera do Leste,MT
-    Março,Show Safra,Tecnologia,18 a 21,Lucas do Rio Verde,MT
-    Março,Força Campo,Agronegócio,20 e 21,Arapoti,PR
-    Março,Super Campo,Agronegócio,21 a 23,Londrina,PR
-    Abril,Tecnoshow Comigo,Tecnologia,07 a 11,Rio Verde,GO
-    Abril,ExpoLondrina,Pecuária,04 a 14,Londrina,PR
-    Abril,Norte Show,Agronegócio,16 a 19,Sinop,MT
-    Abril,Digital Agro,Tecnologia,23 e 24,Carambeí,PR
-    Abril,Agrishow,Tecnologia,28 a 03/05,Ribeirão Preto,SP
-    Maio,AgroBrasília,Agronegócio,20 a 24,Brasília,DF
-    Maio,Bahia Farm Show,Tecnologia,10 a 14,Luís Eduardo Magalhães,BA
-    Maio,Expointer,Pecuária,24 a 01/09,Esteio,RS
-    Junho,Hortitec,Horticultura,19 a 21,Holambra,SP
-    Julho,Feacoop,Cooperativismo,29 a 01/08,Bebedouro,SP
-    Agosto,Show de Inverno,Agronegócio,14 a 16,Ponta Grossa,PR
-    Agosto,Agroshow,Agronegócio,21 a 24,Arapoti,PR
-    Setembro,Agropec,Pecuária,10 a 13,Paragominas,PA
-    Outubro,Frutal,Fruticultura,22 a 24,Fortaleza,CE
-    Novembro,Fenacana,Cana-de-açúcar,19 a 21,Sertãozinho,SP
-    """
+    # CORREÇÃO: A string de dados foi ajustada para garantir a leitura correta pelo pandas.
+    dados_string = """Mês,Evento,Foco,Data,Cidade,UF
+Janeiro,AgroShow Copagril 2026,Agronegócio,14 a 16,Marechal Cândido Rondon,PR
+Janeiro,COOLACER 2026,Tecnologia,28 e 29,Lacerdópolis,SC
+Janeiro,Dinetec,Tecnologia,,Canarana,MT
+Janeiro,Fertilizer Latino Americano,Fertilizantes,,Rio de Janeiro,RJ
+Fevereiro,Sealba Show 2026,Agronegócio,04 a 07,Itabaiana,SE
+Fevereiro,Show Safra BR 163,Tecnologia,18 a 21,Lucas do Rio Verde,MT
+Fevereiro,Show Tecnológico de Verão,Tecnologia,20 a 22,Ponta Grossa,PR
+Fevereiro,Copla Campo,Agronegócio,21 a 23,Piracicaba,SP
+Fevereiro,Show Rural Coopavel,Tecnologia,,"Cascavel",PR
+Março,Expodireto Cotrijal,Agronegócio,03 a 07,Não-Me-Toque,RS
+Março,Expo-Uva,Fruticultura,06 a 09,Jundiaí,SP
+Março,Farm Show,Tecnologia,11 a 14,Primavera do Leste,MT
+Março,Show Safra,Tecnologia,18 a 21,Lucas do Rio Verde,MT
+Março,Força Campo,Agronegócio,20 e 21,Arapoti,PR
+Março,Super Campo,Agronegócio,21 a 23,Londrina,PR
+Abril,Tecnoshow Comigo,Tecnologia,07 a 11,Rio Verde,GO
+Abril,ExpoLondrina,Pecuária,04 a 14,Londrina,PR
+Abril,Norte Show,Agronegócio,16 a 19,Sinop,MT
+Abril,Digital Agro,Tecnologia,23 e 24,Carambeí,PR
+Abril,Agrishow,Tecnologia,28 a 03/05,Ribeirão Preto,SP
+Maio,AgroBrasília,Agronegócio,20 a 24,Brasília,DF
+Maio,Bahia Farm Show,Tecnologia,10 a 14,Luís Eduardo Magalhães,BA
+Maio,Expointer,Pecuária,24 a 01/09,Esteio,RS
+Junho,Hortitec,Horticultura,19 a 21,Holambra,SP
+Julho,Feacoop,Cooperativismo,29 a 01/08,Bebedouro,SP
+Agosto,Show de Inverno,Agronegócio,14 a 16,Ponta Grossa,PR
+Agosto,Agroshow,Agronegócio,21 a 24,Arapoti,PR
+Setembro,Agropec,Pecuária,10 a 13,Paragominas,PA
+Outubro,Frutal,Fruticultura,22 a 24,Fortaleza,CE
+Novembro,Fenacana,Cana-de-açúcar,19 a 21,Sertãozinho,SP
+"""
     
     # Usamos io.StringIO para que o pandas leia a string como se fosse um arquivo
     df = pd.read_csv(io.StringIO(dados_string))
@@ -75,11 +73,11 @@ def geocode_dataframe(df):
     """
     Adiciona colunas de Latitude e Longitude ao DataFrame.
     """
-    geolocator = Nominatim(user_agent="streamlit-app-studio-data-v5")
+    geolocator = Nominatim(user_agent="streamlit-app-studio-data-v6")
     geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
     location_coords = {}
     
-    with st.spinner("Iniciando geocodificação... Isso só será executado na primeira vez e pode levar um minuto."):
+    with st.spinner("A iniciar geocodificação... Isto só será executado na primeira vez e pode levar um minuto."):
         progress_bar = st.progress(0)
         unique_locations = df['Localizacao'].unique()
         total_locations = len(unique_locations)
